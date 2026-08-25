77b77f9f802f111592cab7a6f2ee2592744c5b03
MD Zahin Daiyan <zahindaiyan2004@gmail.com>
2026-08-25 23:19:55 +0600

    Finnaly got task list as a task object making rule/ contract for the object when rechiveng it

--- src/task_tracker/api.py
+++ src/task_tracker/api.py
@@ -10,63 +10,19 @@ from task_tracker.database import (
 
 app = FastAPI()
 
-class TaskCreate(BaseModel):
-    title : str
+class Task(BaseModel):
+  id : int
+  title : str
 
-class TaskUpdate(BaseModel):
-    task_id : int
-    title : str
-
-class TaskDelete(BaseModel):
-    task_id : int
-
-class TaskGetAll(BaseModel):
-    task_id:int
-    title:str
-
-class TaskGet(BaseModel):
-    task_id:int
-
-
-@app.get("/")
-def root():
-    return {"message": "Task Tracker is live"}
-
-@app.get("/tasks",status_code=200)
-def list_tasks(task:TaskGetAll):
+@app.get("/tasks", response_model=list[Task])
+def get_task_list():
     tasks = get_all_tasks()
-    return tasks
-
-@app.post("/tasks", status_code=200)
-def create_task(task:TaskCreate):
-    add_task(task.title)
-    return  {"message" : "Task created successfully"}
-
-@app.get("/tasks/{task_id}" ,status_code=200)
-def get_task_by_id(task:TaskGet):
-    task = get_task(task.task_id)
-    if task:
-        return task
-    else:
-        raise HTTPException(status_code=404, detail="Invalid task id")
-
-@app.put("/tasks/{task_id}", status_code=201)
-def update_task_by_id(task:TaskUpdate):
-    task = get_task(task.task_id)
-    if task:
-        update_task(task.task_id, task.title)
-        return {"message" : "Task updated successfully"}
-    else:
-        raise HTTPException(status_code=404, detail="Task not found")
-
-
-@app.delete("/tasks/{task_id}")
-def delete_task_by_id(task:TaskDelete):
-    task = get_task(task.task_id)
-    if task:
-        delete_task(task.task_id)
-        return {"message" : "Task deleted successfully"}
-    else:
-        raise HTTPException(status_code=404, detail="Task not found")
-
-
+    result = []
+    for task in tasks:
+        result.append(
+           {
+            "id" : task[0],
+            "title" : task[1]
+           }
+        )
+    return result
